e230270508ac76d7af10137bad00fd8c8ea8b613
Hackbright Student <user@mini-004.(none)>
2013-09-24 15:33:09 -0700

    first commit

---
+++ guess.py
@@ -0,0 +1,40 @@
+import random
+
+def generate_number():
+    number = random.randint(1, 100)
+    return number
+
+print "Hello and welcome to the guessing game."
+print "What's your name?"
+name = raw_input("> ")
+print "Hey, %s, what number am I thinking of?" % name
+
+number = generate_number()
+#print number
+not_won = True
+
+counter = 0
+
+while not_won:
+
+    no_guessed = raw_input("> ")
+
+    try:
+        no_guessed = int(no_guessed)
+
+        if no_guessed < number:
+            print "Too low."
+        elif no_guessed > number:
+            print "Too high."
+        else:
+            not_won = False
+            print "You win! Good for you."
+        counter += 1
+    except ValueError:
+        print "Give me an integer!"
+
+
+if counter <= 5:
+    print "You're really good at this!"
+else:
+    print "It took you %d tries. Sucker!" % counter
